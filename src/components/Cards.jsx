449df6249a31febb0a9cd4db9974be171f4747e3
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-02 15:52:18 +0530

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,9 +3,13 @@ import { FaRegFileAlt } from "react-icons/fa";
 
 const Cards = () => {
   return (
-    <div className='bg-zinc-500 w-56 h-72 rounded-3xl p-4 text-white  cursor-pointer'>
+    <div className='relative bg-zinc-500 w-56 h-72 rounded-4xl py-10 px-5 text-white  cursor-pointer overflow-hidden'>
         <FaRegFileAlt />
-        <p className='mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id enim odit dolore dignissimos aliquid cum?</p>
+        <p className='mt-5 font-semibold text-md leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id enim odit dolore dignissimos aliquid cum?</p>
+
+        <div className="w-full h-18 absolute bottom-0 left-0 bg-sky-400">
+
+        </div>
 
     </div>
   )
