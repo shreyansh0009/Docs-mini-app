ea85e3b983eb240e272b21338f01a54965bd1689
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-03 10:45:50 +0530

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa";
 
-const Cards = () => {
+const Cards = ({data}) => {
   return (
     <div className="relative bg-zinc-800 w-56 h-72 rounded-4xl py-10 px-5 text-white   overflow-hidden">
       <FaRegFileAlt />
-      <p className="mt-3 font-semibold text-md leading-tight">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id enim odit
-        dolore dignissimos aliquid cum?
-      </p>
+      <p className="mt-3 font-semibold text-md leading-tight">{data.desc}</p>
 
       <div className="absolute bottom-0 w-full left-0">
         <div className="flex items-center justify-between px-5 w-full h-10 absolute bottom-0 left-0 mb-14">
-          <h5>0.4mb</h5>
+          <h5>{data.size}</h5>
           <span className="pr-2 cursor-pointer">
             <FaDownload />
           </span>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Cards from './Cards'
+import React from "react";
+import Cards from "./Cards";
 
 function Foreground() {
+  const data = [
+    {
+      desc: "This is first card, Lorem ipsum dolor sit amet.",
+      size: "0.9mb",
+      close: true,
+      tag: {
+        isOpen: false,
+        tagTitle: "Download",
+        tagColor: "green",
+      },
+    },
+  ];
   return (
     <div className="fixed z-[3] w-full h-full top-0 left-0">
-
-        <Cards />
-
+      {data.map((item, idx) => (
+        <Cards data={item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
