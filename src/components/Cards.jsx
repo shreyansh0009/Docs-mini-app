d3f832f1efb41cbe04a1a54a9a03d9a1866769c7
shreyansh0009 <saurabhsharraf@gmail.com>
2025-04-03 10:55:28 +0530

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa";
+import { IoMdCloseCircle } from "react-icons/io";
 
-const Cards = ({data}) => {
+const Cards = ({ data }) => {
   return (
     <div className="relative bg-zinc-800 w-56 h-72 rounded-4xl py-10 px-5 text-white   overflow-hidden">
       <FaRegFileAlt />
@@ -12,15 +13,15 @@ const Cards = ({data}) => {
         <div className="flex items-center justify-between px-5 w-full h-10 absolute bottom-0 left-0 mb-14">
           <h5>{data.size}</h5>
           <span className="pr-2 cursor-pointer">
-            <FaDownload />
+            {data.close ? <IoMdCloseCircle size="1.3em" /> : <FaDownload />}
           </span>
         </div>
 
-        <div className="w-full h-14 bg-green-500 flex items-center justify-center">
-
-          <h3 className="text-sm font-semibold">Download Now</h3>
-
-        </div>
+        {data.tag.isOpen ? (
+          <div className="w-full h-14 bg-green-500 flex items-center justify-center">
+            <h3 className="text-sm font-semibold"></h3>
+          </div>
+        ) : null}
       </div>
     </div>
   );
